778fa83d01d202f40d438d085dae504b682e57df
Pedro Caetano <pedroccaetano@live.com>
2020-04-21 00:21:44 -0300

    Challenge finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getCustomRepository(CategoriesRepository);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('Insufficient funds', 400);
+    }
+
+    const checkCategoryExits = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    const { id: category_id } =
+      checkCategoryExits ||
+      (await categoryRepository.save(
+        await categoryRepository.create({ title: category }),
+      ));
+
+    const transaction = await transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
